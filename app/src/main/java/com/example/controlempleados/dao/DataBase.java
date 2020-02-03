87e092b7546ed6f692a38ddfda478a29087a7028
dam <maxim.macari.dam@gmail.com>
2020-02-03 17:01:25 +0100

    a

--- app/src/main/java/com/example/controlempleados/dao/DataBase.java
+++ app/src/main/java/com/example/controlempleados/dao/DataBase.java
@@ -8,13 +8,16 @@ import androidx.annotation.Nullable;
 
 public class DataBase extends SQLiteOpenHelper {
 
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Workers.db";
+
     public DataBase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
