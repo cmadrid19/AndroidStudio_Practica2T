15a03312db2fd9248e49c7f43faa1b1505272d6e
dam <maxim.macari.dam@gmail.com>
2020-02-13 20:36:01 +0100

    agit push

--- app/src/main/java/com/example/controlempleados/activities/AcercaDeActivity.java
+++ app/src/main/java/com/example/controlempleados/activities/AcercaDeActivity.java
@@ -3,8 +3,11 @@ package com.example.controlempleados.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.controlempleados.R;
+
+import org.w3c.dom.Text;
 
 public class AcercaDeActivity extends AppCompatActivity {
 
@@ -12,5 +15,20 @@ public class AcercaDeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+
+        TextView textView = findViewById(R.id.info);
+
+
+
+        //TODO
+        /*
+        1. Añadiir un file interno con información del proyecto
+        2. Leer el fichero y meterlo en el Text View
+         */
+
+
+
+        textView.setText("");
+
     }
 }
