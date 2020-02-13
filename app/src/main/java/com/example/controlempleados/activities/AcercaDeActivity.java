d34d5e524ef7a89c9ba545aa5b61b1ca4a5ffc82
dam <maxim.macari.dam@gmail.com>
2020-02-13 20:42:50 +0100

    agit push

--- app/src/main/java/com/example/controlempleados/activities/AcercaDeActivity.java
+++ app/src/main/java/com/example/controlempleados/activities/AcercaDeActivity.java
@@ -9,6 +9,9 @@ import com.example.controlempleados.R;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class AcercaDeActivity extends AppCompatActivity {
 
     @Override
@@ -19,7 +22,6 @@ public class AcercaDeActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.info);
 
 
-
         //TODO
         /*
         1. Añadiir un file interno con información del proyecto
@@ -27,8 +29,34 @@ public class AcercaDeActivity extends AppCompatActivity {
          */
 
 
-
         textView.setText("");
+
+    }
+
+
+    private void readFile() {
+
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        tv1.setText("");
+        try {
+            //Abre un flujo desde la carpeta raw donde se encuentra un fichero.txt
+            BufferedReader fichero = new BufferedReader(new InputStreamReader(getResources().openRawResource(R.raw.fichero)));
+            String lectura = fichero.readLine();
+
+            //Le su contenido y lo mete en un StringBuilder
+            StringBuilder sb = new StringBuilder();
+            while (lectura != null) {
+                sb.append(lectura + "\n");
+                lectura = fichero.readLine();
+            }
+
+            //muestra el stringBuilder
+            tv1.setText(sb);
+            //Cierrra el flujo/fichero
+            fichero.close();
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
 
     }
 }
